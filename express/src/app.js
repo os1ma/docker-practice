eef2e968176ab2be6c7a53c08b545b86f92dc6a9
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-05 02:00:27 +0000

    Express から MySQL に接続するコードを追加

--- express/src/app.js
+++ express/src/app.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const mysql = require('mysql');
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello Express!')
+})
+
+app.get('/users', (req, res) => {
+  const connection = mysql.createConnection({
+    host     : 'my-db',
+    user     : 'root',
+    password : 'root',
+    database : 'my_db'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT id, name FROM users', (error, results) => {
+    if (error) throw error;
+
+    console.log('results : ', results);
+
+    res.send(results)
+  });
+
+  connection.end();
 })
 
 app.listen(port, () => {
